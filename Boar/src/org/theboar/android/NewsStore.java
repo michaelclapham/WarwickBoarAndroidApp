3d0cbd5d6cd2c38a13239eede4f2eef189b2f010
Michael I.J. Clapham <michaeljclapham@gmail.com>
2014-03-05 01:51:20 +0000

    messy image cache using sd card
    Very messy code to download images to sd card to save bandwidth when
    loading the app.

--- Boar/src/org/theboar/android/NewsStore.java
+++ Boar/src/org/theboar/android/NewsStore.java
@@ -3,6 +3,8 @@ package org.theboar.android;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -24,10 +26,12 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Environment;
 import android.text.format.DateFormat;
 import android.text.format.DateUtils;
@@ -155,8 +159,11 @@ public class NewsStore implements INewsStore {
 					head.setHeadlineTitle(storyTitle);
 					//String imageURL = story.getJSONArray("attachments").getJSONObject(0).getJSONObject("images").getJSONObject("full").getString("url");
 					String imageURL = story.getJSONObject("thumbnail_images").getJSONObject("medium").getString("url");
+					
+					head.setLowResImage(getSmartDrawableFromUrl(imageURL));
+					
 					//String imageURL = "http://theboar.org/wp-content/uploads/2014/02/Dating.jpg";
-					head.setLowResImage(drawableFromUrl(imageURL));
+					//
 					String dateTimeString = story.getString("date");
 					SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-DD HH:mm:ss", Locale.ENGLISH);
 					Date datePublished = df.parse(dateTimeString);
@@ -169,9 +176,6 @@ public class NewsStore implements INewsStore {
 				}
 				list.setDoneLoading(true);
 			} catch (JSONException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			} catch (ParseException e) {
@@ -217,6 +221,93 @@ public class NewsStore implements INewsStore {
 
         x = BitmapFactory.decodeStream(input);
         return new BitmapDrawable(x);
+    }
+	
+	public static Bitmap bitmapFromUrl(String url) throws IOException {
+        Bitmap x;
+
+        HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+        connection.connect();
+        InputStream input = connection.getInputStream();
+
+        x = BitmapFactory.decodeStream(input);
+        return x;
+    }
+	
+	public Drawable getSmartDrawableFromUrl(String url){
+		Drawable dr = null;
+		String sdState = Environment.getExternalStorageState();
+		if( sdState.equalsIgnoreCase(Environment.MEDIA_MOUNTED)){
+			// Now check local storage for pre-cached images
+			File DataStorage = new File(
+					Environment.getExternalStorageDirectory(),"WarwickBoar/images/url-cache");
+			if (!DataStorage.exists()) {
+				if (!DataStorage.mkdirs())
+					Log.d("Print","failed to create directory");
+			} else Log.d("Print","Directory Exists");
+
+			String fileName = Uri.parse(url).getLastPathSegment();
+			Log.v(this.toString(), "FILE NAME:: " + fileName);
+			File[] files = DataStorage.listFiles();
+			
+			for(int i = 0; i < files.length; i++){
+				// Image is not in SD card cache
+				if(fileName.equalsIgnoreCase(files[i].getName())){
+					// Now use the image that was cached or just downloaded to create a drawable
+					FileInputStream fis;
+					try {
+						Log.i(this.toString(),"LOADING " + fileName + "FROM FILE...");
+						fis = new FileInputStream(DataStorage
+								+ File.separator + files[i].getName());
+						dr = Drawable.createFromStream(fis,"news2");
+						return dr;
+					} catch (FileNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				
+			}
+			// Then download the image
+			downloadFile(url,fileName,"/WarwickBoar/images/url-cache/");
+		}
+		
+		// Use old method in case of pathetic failure or SD card being unavailable
+		try {
+			return drawableFromUrl(url);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return dr;
+	}
+	
+	public void downloadFile(String fileURL, String fileName, String directory) {
+        try {
+            URL u = new URL(fileURL);
+            HttpURLConnection c = (HttpURLConnection) u.openConnection();
+            c.setRequestMethod("GET");
+            c.setDoOutput(true);
+            c.connect();
+            File sdCard = Environment.getExternalStorageDirectory();
+            File dir = new File (sdCard.getAbsolutePath() + directory);
+            dir.mkdirs();
+            File file = new File(dir, fileName);
+
+            FileOutputStream f = new FileOutputStream(file);
+
+            InputStream in = c.getInputStream();
+
+            byte[] buffer = new byte[1024];
+            int len1 = 0;
+            while ((len1 = in.read(buffer)) > 0) {
+                f.write(buffer, 0, len1);
+            }
+            f.close();
+        } catch (Exception e) {
+            Log.d("Downloader", e.getMessage());
+        }
+
     }
 
 }
